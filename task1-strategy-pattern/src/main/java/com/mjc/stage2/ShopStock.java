a80bbfd8ba6a0cb3d48411e5eabdb43d3a76a911
nubik132 <mister.nubik@list.ru>
2023-12-10 13:13:23 +0300

    fix

--- task1-strategy-pattern/src/main/java/com/mjc/stage2/ShopStock.java
+++ task1-strategy-pattern/src/main/java/com/mjc/stage2/ShopStock.java
@@ -1,6 +1,5 @@
 package com.mjc.stage2;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,7 +10,8 @@ public class ShopStock {
         this.productList = productList;
     }
 
-    List<Product> executeFilteringStrategy(FilteringStrategy filteringStrategy){
+    public List<Product> executeFilteringStrategy(FilteringStrategy filteringStrategy){
+
         return productList.stream()
                 .filter(filteringStrategy::filter)
                 .collect(Collectors.toList());
