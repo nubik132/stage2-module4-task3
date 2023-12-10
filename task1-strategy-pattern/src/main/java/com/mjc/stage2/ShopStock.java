94fca35d81862e8c64dd4e3729ad732079fd5e78
nubik132 <mister.nubik@list.ru>
2023-12-10 13:08:42 +0300

    complete task

--- task1-strategy-pattern/src/main/java/com/mjc/stage2/ShopStock.java
+++ task1-strategy-pattern/src/main/java/com/mjc/stage2/ShopStock.java
@@ -1,6 +1,8 @@
 package com.mjc.stage2;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ShopStock {
     private final List<Product> productList;
@@ -9,5 +11,10 @@ public class ShopStock {
         this.productList = productList;
     }
 
-    // Write your code here!
+    List<Product> executeFilteringStrategy(FilteringStrategy filteringStrategy){
+        return productList.stream()
+                .filter(filteringStrategy::filter)
+                .collect(Collectors.toList());
+    }
+
 }
